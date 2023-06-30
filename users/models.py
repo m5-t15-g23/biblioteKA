b36468c68b9c98a67a8af5c2e4e935f88e3ad188
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-06-30 11:13:09 -0300

    feat: add model and serializer for users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    is_colaborator = models.BooleanField(null=True, default=False)
+    status_for_loan = models.BooleanField(null=True, default=True)
+
+    def __str__(self) -> str:
+        return f"<User [{self.id}] -> {self.first_name}>"
---
+++ users/serializers.py
@@ -0,0 +1,62 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import User
+
+
+class UserSerializer(serializers.ModelField):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "username",
+            "first_name",
+            "last_name",
+            "password",
+            "is_colaborator",
+            "status_for_loan"
+        ]
+        read_only_fields = ["id"]
+        extra_kwargs = {
+            "email": {
+                "validators": [
+                    UniqueValidator(
+                        queryset=User.objects.all(),
+                        message="Email already in use"
+                    )
+                ]
+            },
+            "username": {
+                "validators": [
+                    UniqueValidator(
+                        queryset=User.objects.all(),
+                        message="Username already in use"
+                    )
+                ]
+            },
+            "password": {"write_only": True}
+        }
+
+    def create(self, validated_data: dict) -> User:
+        is_colaborator = validated_data["is_colaborator"]
+
+        user = (
+            User.objects.create_superuser(**validated_data)
+            if is_colaborator is True
+            else User.objects.create_user(**validated_data)
+        )
+
+        return user
+
+    def update(self, instance: User, validated_data: dict) -> User:
+        new_password = validated_data.pop("password", None)
+        user = instance
+
+        if new_password:
+            user.set_password(new_password)
+
+        for key, value in validated_data.items():
+            setattr(user, key, value)
+
+        user.save()
+        return user
