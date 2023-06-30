5e1b94517139b7937d7eaf1f7ac8d52c6820e93e
isabelpaiva <isxpaiva@icloud.com>
2023-06-30 16:29:01 -0300

    feat(loans): implement model, serializer and view

--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,17 @@
+from datetime import datetime, timedelta
 from django.db import models
 
-# Create your models here.
+def return_date():    
+    return datetime.now() + timedelta(days=30)
+
+class Loan(models.Model):
+    
+    loan_date = models.DateTimeField(auto_now_add=True)
+    loan_return = models.DateTimeField(default=return_date)
+    
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="loan"
+    ) 
+    copie = models.ForeignKey(
+        "copie.Copie", on_delete=models.PROTECT, related_name="copie"
+    )
--- loans/views.py
+++ loans/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from loans.models import Loan
+from loans.serializers import LoanSerializer
+from django.shortcuts import get_object_or_404
+from copies.models import Copy
 
-# Create your views here.
+
+class LoanView(generics.ListCreateAPIView):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+
+    def perform_create(self, serializer):
+      copy_id = self.kwargs.get("pk")
+      copy = get_object_or_404(Copy, pk=copy_id)
+
+      serializer.save(
+         user=self.request.user, 
+         copy_id = copy.id
+      )
