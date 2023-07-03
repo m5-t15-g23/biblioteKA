b72619db82da594bd4865495b9898a4c744c358b
dnapolitanno <dudadnapolitano@gmail.com>
2023-07-03 16:05:18 -0300

    fix: correct line identation

--- loans/urls.py
+++ loans/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("loans/", views.LoanView.as_view()),
     path("loans/<int:copy_id>/", views.LoanCopyDetailView.as_view()),
-    path("loans/student/<int:student_id>/", views.LoanColaboratorDetailView.as_view()),
+    path("loans/student/<int:student_id>/",
+         views.LoanColaboratorDetailView.as_view()),
 ]
