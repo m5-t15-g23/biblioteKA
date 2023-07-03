5199b2d6aeb49be673f10d381ea5c2d66549300e
dnapolitanno <dudadnapolitano@gmail.com>
2023-07-03 16:41:38 -0300

    feat: implent authentications

---
+++ users/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework import permissions
+from rest_framework.views import View
+
+
+class IsAuthenticated(permissions.BasePermission):
+    def has_permission(self, request, view: View):
+        return request.user.is_authenticated
+
+
+class IsColaborator(permissions.BasePermission):
+    def has_permission(self, request, view: View):
+        return request.user.is_authenticated and request.user.is_colaborator
+
+
+class IsColaboratorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view: View):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return request.user.is_authenticated and request.user.is_colaborator
