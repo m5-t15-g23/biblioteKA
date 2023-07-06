c1008aa9ca1d9bdf4dbe0dc09dc813127cf66bb5
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-06 18:36:18 -0300

    fix(emails): fix the general configs

--- biblioteka/settings.py
+++ biblioteka/settings.py
@@ -160,9 +160,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_USE_TLS = True
+EMAIL_USE_TLS = True
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_SSL = False
