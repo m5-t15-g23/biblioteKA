1943d3f57b47b0eecad742741fe0453e5bd7a7fe
dnapolitanno <dudadnapolitano@gmail.com>
2023-07-07 19:12:08 -0300

    feat(book): implement detail view to list by id

--- books/views.py
+++ books/views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from users.permissions import IsColaboratorOrReadOnly
 
 from .models import Book
 from .serializers import BookSerializer
+from users.permissions import IsColaboratorOrReadOnly, IsAuthenticated
 
 
 class BookView(generics.ListCreateAPIView):
@@ -17,3 +17,16 @@ class BookView(generics.ListCreateAPIView):
         user = self.request.user
 
         serializer.save().users.add(user)
+
+
+class BookDetailView(generics.RetrieveAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    serializer_class = BookSerializer
+    lookup_url_kwarg = "book_id"
+
+    def get_queryset(self):
+        book_id = self.kwargs.get("book_id")
+
+        return Book.objects.filter(id=book_id)
--- users/exceptions.py
+++ users/exceptions.py
@@ -4,4 +4,3 @@ from rest_framework.exceptions import APIException
 class StudentLoanException(APIException):
     status_code = 406
     default_code = "service_unavailable"
-
