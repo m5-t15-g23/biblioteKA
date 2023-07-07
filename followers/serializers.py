cadc1f6ea85cd8235b6fe2a6de8b1926cf4bb4e4
dnapolitanno <dudadnapolitano@gmail.com>
2023-07-07 19:25:58 -0300

    fix(follower): fix representation return for list

--- followers/serializers.py
+++ followers/serializers.py
@@ -4,6 +4,15 @@ from .models import Follower
 
 
 class FollowerSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        return {
+            "id": instance.id,
+            "student_id": instance.student.id,
+            "student_username": instance.student.username,
+            "book_followed_id": instance.book_followed.id,
+            "book_followed_title": instance.book_followed.title,
+        }
+
     class Meta:
         model = Follower
         fields = [
