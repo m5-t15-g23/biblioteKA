859fa5648cf6d927fd5f672885586358301ee579
dnapolitanno <dudadnapolitano@gmail.com>
2023-07-07 18:40:52 -0300

    docs: implement docs biblioteka API

--- biblioteka/urls.py
+++ biblioteka/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +25,6 @@ urlpatterns = [
     path("api/", include("copies.urls")),
     path("api/", include("loans.urls")),
     path("api/", include("followers.urls")),
-
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/redoc/", SpectacularRedocView.as_view())
 ]
