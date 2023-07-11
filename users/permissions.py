c215fd2a95622be24aa2779bfa6eadec670d0248
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-11 12:41:20 -0300

    feat(permissions): add missing type hint

--- users/permissions.py
+++ users/permissions.py
@@ -1,35 +1,35 @@
 from rest_framework import permissions
-from rest_framework.views import View
+from rest_framework.views import View, Request
 
 from loans.models import Loan
 
 
 class IsAuthenticated(permissions.BasePermission):
-    def has_permission(self, request, view: View):
+    def has_permission(self, request: Request, view: View) -> bool:
         return request.user.is_authenticated
 
 
 class IsColaborator(permissions.BasePermission):
-    def has_permission(self, request, view: View):
+    def has_permission(self, request: Request, view: View) -> bool:
         return request.user.is_authenticated and request.user.is_colaborator
 
 
 class IsColaboratorOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view: View):
+    def has_permission(self, request: Request, view: View) -> bool:
         if request.method in ['GET', 'HEAD', 'OPTIONS']:
             return True
         return request.user.is_authenticated and request.user.is_colaborator
 
 
 class IsAuthenticatedOrCreate(permissions.BasePermission):
-    def has_permission(self, request, view: View):
+    def has_permission(self, request: Request, view: View) -> bool:
         if request.method == 'POST':
             return True
         return request.user.is_authenticated
 
 
 class IsStudent(permissions.BasePermission):
-    def has_permission(self, request, view: View):
+    def has_permission(self, request: Request, view: View) -> bool:
         return (request.user.is_authenticated and
                 request.user.is_colaborator is False)
 
